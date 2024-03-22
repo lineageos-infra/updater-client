bc1a1ae0d912275dfa7176386ff09806e6f6f72e
Cosmin Tanislav <demonsingur@gmail.com>
2024-03-22 19:13:03 +0200

    Move svg styling out of file

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,14 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import svgLoader from 'vite-svg-loader'
 
 // https://vite.dev/config/
 export default defineConfig({
   build: {
     sourcemap: true
   },
-  plugins: [vue(), tailwindcss()],
+  plugins: [vue(), tailwindcss(), svgLoader()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
