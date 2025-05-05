9634cca8320a76162c8ba98c3ebb4920d09777ea
LuK1337 <priv.luk@gmail.com>
2025-05-06 00:44:40 +0200

    Upgrade to ESLint v9

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import configPrettier from 'eslint-config-prettier/flat'
+import globals from 'globals'
+import { includeIgnoreFile } from '@eslint/compat'
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const gitignorePath = path.resolve(__dirname, '.gitignore')
+
+export default [
+  {
+    files: ['**/*.vue', '**/*.js', '**/*.jsx', '**/*.cjs', '**/*.mjs'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      },
+      parserOptions: {
+        ecmaVersion: 'latest'
+      }
+    }
+  },
+  includeIgnoreFile(gitignorePath),
+  ...pluginVue.configs['flat/essential'],
+  js.configs.recommended,
+  configPrettier
+]
