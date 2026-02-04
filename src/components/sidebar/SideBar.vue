ccdd551d7611e9c8554a42e5ef89fb1488d63bb2
Nicholas Lim <niclimcongyi@gmail.com>
2026-02-04 15:06:27 +0800

    SideBar: Use Vue composition API

--- src/components/sidebar/SideBar.vue
+++ src/components/sidebar/SideBar.vue
@@ -1,51 +1,18 @@
 <template>
-  <div class="sidebar">
-    <router-link to="/">
-      <div class="logo">
-        <img src="../../assets/sidebar-logo.svg" alt="LineageOS Logo" />
+  <div class="sidebar shadow-[0 1px 3px rgba(0, 0, 0, 0.24)] relative flex flex-col">
+    <RouterLink to="/">
+      <div class="logo bg-brand-primary flex h-25 shrink-0 items-center justify-center">
+        <img class="h-10" src="../../assets/sidebar-logo.svg" alt="LineageOS Logo" />
       </div>
-    </router-link>
-    <device-selector :active-model="activeModel"></device-selector>
+    </RouterLink>
+    <DeviceSelector :active-model="props.activeModel" />
   </div>
 </template>
 
-<script>
+<script setup>
 import DeviceSelector from '../device-selector/DeviceSelector.vue'
 
-export default {
-  name: 'SideBar',
-  components: {
-    DeviceSelector
-  },
-  props: {
-    activeModel: String
-  }
-}
+const props = defineProps({
+  activeModel: String
+})
 </script>
-
-<style scoped>
-.sidebar {
-  display: flex;
-  flex-direction: column;
-
-  position: relative;
-
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.24);
-}
-
-.sidebar .logo {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  flex-shrink: 0;
-
-  background: #167c80;
-
-  height: 100px;
-}
-
-.sidebar .logo img {
-  height: 40px;
-}
-</style>
