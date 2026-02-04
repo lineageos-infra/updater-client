f4559e29f67b29bc810f926c6ec5821f0c992093
Nicholas Lim <niclimcongyi@gmail.com>
2026-02-05 04:27:06 +0800

    Use tailwind vite plugin instead of postcss

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,14 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     sourcemap: true
   },
-  plugins: [vue()],
+  plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
