80cbf150018c968b733e4c5d24329a7989fcb740
Nicholas Lim <niclimcongyi@gmail.com>
2026-02-08 03:03:43 +0800

    Import Vue TypeScript boiler plate

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   build: {
     sourcemap: true
