846f33740edc7eb13dafec8c421aa48bf00b7e4b
Nicholas Lim <niclimcongyi@gmail.com>
2026-02-16 16:49:53 +0800

    src: Move global styles to style.css

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,9 @@
 <template>
-  <RouterView id="sidebar" name="sidebar"></RouterView>
-  <RouterView id="main" name="main"></RouterView>
-  <HorizontalLoader id="loader" />
+  <RouterView name="sidebar"></RouterView>
+  <RouterView id="main" class="h-full w-full grow" name="main"></RouterView>
+  <HorizontalLoader class="absolute bottom-0 left-0" />
 </template>
 
 <script setup lang="ts">
 import HorizontalLoader from './components/utils/HorizontalLoader.vue'
 </script>
-
-<style>
-#app {
-  height: 100%;
-  width: 100%;
-
-  display: flex;
-
-  font-family: 'Roboto', sans-serif;
-}
-
-#sidebar {
-  width: 300px;
-  max-width: 30%;
-  min-width: 128px;
-  height: 100%;
-}
-
-#main {
-  flex-grow: 1;
-  height: 100%;
-  width: 100%;
-}
-
-#loader {
-  position: absolute;
-  bottom: 0;
-  left: 0;
-}
-
-@media (max-width: 1024px) {
-  #sidebar {
-    display: none;
-  }
-}
-</style>
