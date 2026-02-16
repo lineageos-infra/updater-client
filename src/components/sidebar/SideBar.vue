b59b7b24111948599a39b1bfc6bc3f08480d4028
Nicholas Lim <niclimcongyi@gmail.com>
2026-02-16 16:49:53 +0800

    src: Move global styles to style.css

--- src/components/sidebar/SideBar.vue
+++ src/components/sidebar/SideBar.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="sidebar shadow-[0 1px 3px rgba(0, 0, 0, 0.24)] relative flex flex-col">
+  <div
+    class="shadow-[0 1px 3px rgba(0, 0, 0, 0.24)] relative hidden h-full w-75 max-w-[30%] min-w-32 flex-col lg:flex"
+  >
     <RouterLink to="/">
       <div class="logo bg-brand-primary flex h-25 shrink-0 items-center justify-center">
         <img class="h-10" src="../../assets/sidebar-logo.svg" alt="LineageOS Logo" />
