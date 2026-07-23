1e4f36a22442a17cfa30ff77994567f648db5ea5
LuK1337 <priv.luk@gmail.com>
2026-07-23 17:42:16 +0200

    Move sane defaults from app01 and src/services/config.ts to global .env

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,9 +1,7 @@
 export const API_HOSTNAME = import.meta.env.VITE_API_HOSTNAME
-export const BRAND_NAME = import.meta.env.VITE_BRAND_NAME ?? 'LineageOS'
+export const BRAND_NAME = import.meta.env.VITE_BRAND_NAME
 export const WIKI_URL = import.meta.env.VITE_WIKI_URL
-export const SIGNING_KEY_FINGERPRINT =
-  import.meta.env.VITE_SIGNING_KEY_FINGERPRINT ??
-  '72:96:32:27:d6:6c:4c:4d:5f:a0:91:6a:c2:2c:79:3c:d4:5f:43:5c'
+export const SIGNING_KEY_FINGERPRINT = import.meta.env.VITE_SIGNING_KEY_FINGERPRINT
 export const BRAND_COLOR_LIGHT = import.meta.env.VITE_BRAND_COLOR_LIGHT
 export const BRAND_COLOR_DARK = import.meta.env.VITE_BRAND_COLOR_DARK
 export const BRAND_COLOR_PRIMARY = import.meta.env.VITE_BRAND_COLOR_PRIMARY
